bcec2def1638e8f6f63b35b9046baef659c2aa29
Roman Kulev <rk1126@its.york.ac.uk>
2023-10-02 11:01:17 +0100

    wordcount made better

--- Programming/wordcount/src/main.c
+++ Programming/wordcount/src/main.c
@@ -2,23 +2,38 @@
 #include <stdio.h>
 #include <string.h>
 
+static int isAlphanum(char *string) {
+	return ((*string >= 'A' && *string <= 'Z') || (*string >= 'a' && *string <= 'z') || (*string >= '0' && *string <= '9'));
+}
+
 static int wordcount(char * string) {
 	int words = 0;
-	char character, prevChar = ' ';
-	while (*string != '\0') {
-		character = *string;
-		if ((character >= 'A' && character <= 'Z') || (character >= 'a' && character <= 'z') || (character >= '0' && character <= '9')) {
-			// is alphanumeric
-		}
-		else if (character == ' ' || character == '.' || character == ',' || character == '!' || character == '?') {
-			if (prevChar != ' ' && prevChar != '.' && prevChar != ',' && prevChar != '!' && prevChar != '?') {
-				words += 1;
+	while (*string) {
+		if (isAlphanum(string)) {
+			if (!isAlphanum(string+1)) {
+				words++;
 			}
 		}
-		prevChar = character;
 		string++;
 	}
-	
+	return words;
+}
+
+static int wordcount2(char * string) {
+	int inWord = 0;
+	int words = 0;
+	while(*string) {
+		if (isAlphanum(string)) {
+			if (!inWord) {
+				inWord = 1;
+				words++;
+			} 
+		}
+		else {
+			inWord = 0;
+		}
+		string++;
+	}
 	return words;
 }
 
@@ -28,9 +43,10 @@ int main(void) {
 	configUSART2(38400);
 	
 	// This text contains 11 words.
-	char * text = "This is a word count test.\nHere is the second line !";
+	char * text = "  ,This is a word count test.\nHere is the second line oookie helloooo!";
 	printf("The text is:\n%s\n", text);
 	printf("It contains %d words.\n", wordcount(text));
+	printf("It contains %d words.\n", wordcount2(text));
 
 	while(1);
 }
