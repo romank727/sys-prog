1ab17c5f7774bdd7deafdeb979b2b095e3fbedda
Roman Kulev <rk1126@its.york.ac.uk>
2023-10-09 19:20:11 +0100

    makefile stuff

---
+++ makefile test/heap.c
@@ -0,0 +1,28 @@
+#include "heap.h"
+
+static void heap_up(heap_t *heap) {
+	// Write me!
+}
+
+static void heap_down(heap_t *heap) {
+	// Write me!
+}
+
+void heap_insert(heap_t *heap, int32_t value) {
+	// The new element is always added to the end of a heap
+	heap->store[(heap->size)++] = value;
+	heap_up(heap);
+}
+
+int32_t heap_extract(heap_t *heap) {
+	// The root value is extracted, and the space filled by the value from the end
+	// If the heap is empty, this will fail horribly...
+	int value = heap->store[0];
+	heap->store[0] = heap->store[--(heap->size)];
+	heap_down(heap);
+	return value;
+}
+
+uint_fast8_t heap_isEmpty(heap_t *heap) {
+	return !(heap->size);
+}
