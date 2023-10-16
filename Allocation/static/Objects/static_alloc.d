2619fd41736459efa4ea37e1093867c766eb62d3
Roman Kulev <rk1126@its.york.ac.uk>
2023-10-16 16:51:57 +0100

    todo: mempool

--- Allocation/static/Objects/static_alloc.d
+++ Allocation/static/Objects/static_alloc.d
@@ -1,3 +1,4 @@
 ./objects/static_alloc.o: src\static_alloc.c inc\static_alloc.h \
   c:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stddef.h \
-  c:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdint.h
+  c:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdint.h \
+  c:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdio.h
