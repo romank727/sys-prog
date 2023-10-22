6b7ceb9214b332423efa555eb8e27382da43563f
Roman Kulev <rk1126@its.york.ac.uk>
2023-10-22 20:12:33 +0100

    finish fibonacci

--- Assembly/fibonacci/asm/main.s
+++ Assembly/fibonacci/asm/main.s
@@ -7,7 +7,21 @@ array_end
 	EXPORT asm_main
 asm_main
 	; Your code goes here
+	
+	LDR r0, =array
+	LDR r1, =array_end
+	
+	MOV r2, #1
+	MOV r3, #1
 
+loop
+	STR r2, [r0], #4	; 1, 		1, 2, 	1, 2, 3,	1, 2, 3, 5,		1, 2, 3, 5, 8,		1, 2, 3, 5, 8, 13,   	.....
+	MOV r4, r2			; r4 = 1	r4 = 2	r4 = 3		r4 = 5			r4 = 8				r4 = 13
+	ADD r2, r3			; r2 = 2	r2 = 3	r2 = 5		r2 = 8			r2 = 13				r2 = 21
+	MOV r3, r4			; r3 = 1	r3 = 2	r3 = 3		r3 = 5			r3 = 8				r3 = 13
+	CMP r1, r0
+	BHS loop
+	
 	B .			; Loop forever
 
 	ALIGN
--- Assembly/mult/asm/main.s
+++ Assembly/mult/asm/main.s
@@ -2,12 +2,6 @@
 	EXPORT asm_main
 asm_main
 
-	; Your instructions go here...
-	
-	; multiply by 8 (LSL by 3)
-	; multiply by 4 (LSL by 2)
-	; add 1 instance of original number -> = 13x
-	
 	MOV r1, r0		; copy original number into r1
 	LSL r0, r0, #3	; multiply by 8 (shift left by 3)
 	LSL r1, r1, #2	; multiply by 4 (shift left by 2)
