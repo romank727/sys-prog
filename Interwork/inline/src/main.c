636612f05527fd6df80fef13f2ebb96646dc3435
Andy Pomfret <andrew.pomfret@york.ac.uk>
2023-10-30 13:51:44 +0000

    Update inline/src/main.c

--- Interwork/inline/src/main.c
+++ Interwork/inline/src/main.c
@@ -8,12 +8,11 @@ int main(void) {
 	configUSART2(38400);
 
 	uint32_t x=3, y=5, z=0;
-	__asm volatile (
-		"ADD r0, %[in1], %[in2], LSL #3\n\t" /* x + y*8 */
-		"ADD %[output], r0, %[in1], LSL #2" /* +(x*4) = x*5 + y*8 */
-		: [output] "=&r" (z)
-		: [in1] "r" (x), [in2] "r" (y)
-		: "r0"
+  __asm volatile (
+    "ADD %[output], %[input], %[input], LSL #3\n\t" /* x*9 */
+    "ADD %[output], %[output], %[input], LSL #2" /* +(x*4) */
+    : [output] "=&r" (y)
+    : [input] "r" (x)
 	);
 
 	printf("z = %" PRIu32 "\n", z);
