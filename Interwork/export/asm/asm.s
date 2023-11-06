b6ad262300a29539baab4b68951795a915d88f73
Roman Kulev <rk1126@york.ac.uk>
2023-11-06 01:44:24 +0000

    todo: first task

--- Interwork/export/asm/asm.s
+++ Interwork/export/asm/asm.s
@@ -1,5 +1,12 @@
 	AREA mainarea,CODE
+	EXPORT calculate
 
-	; Your instructions go here...
-
+calculate
+	STMFD sp!, {r4, lr}
+	MOV r2, r0, LSL #2
+	ADD r2, r2, r0
+	ADD r0, r2, r1
+	LDMFD sp!, {r4, pc}
+	
+	
 	END
---
+++ Interwork/import/Objects/hardfault.d
@@ -0,0 +1,3 @@
+./objects/hardfault.o: src\Utils\hardfault.c \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdint.h \
+  C:\Users\Roman\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\cmsis_armclang.h
